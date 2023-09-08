27fcd52c35b7567edfb9e54d8e6ca4174850e30d
Arya Priema <99aryaprima@gmail.com>
2023-09-08 22:06:19 +0700

    uploads file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const multer = require("multer");
+
+const storage = multer.diskStorage({ destination: (req, file, cb) => {
+    cb(null, './src/uploads/');
+}, filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+} })
+const upload = multer({ storage })
 
 const app = express();
 app.use(express.json());
@@ -7,6 +15,7 @@ app.get("/", (req, res) => {
   res.send("/");
 });
 
+// User
 app.get("/user", (req, res) => {
   const user = [
     {
@@ -38,6 +47,11 @@ app.delete("/user/:id", (req, res) => {
   console.log(req.params.id);
 
   res.send("Deleted Succesfuly")
+});
+
+// Recipes
+app.post("/recipes", upload.single("file"), (req, res) => {
+    res.send("Upload Succesfuly")
 });
 
 app.listen(3000, () => {
